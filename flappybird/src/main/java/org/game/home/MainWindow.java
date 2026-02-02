b442bee05cb7b03295cfff3c01f255c4a9406cb2
The Guitar <turanlizad776@gmail.com>
2026-02-02 20:48:32 +0400

    a1

--- flappybird/src/main/java/org/game/home/MainWindow.java
+++ flappybird/src/main/java/org/game/home/MainWindow.java
@@ -1,19 +1,41 @@
 package org.game.home;
 
 import java.awt.Dimension;
+import java.nio.file.Paths;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class MainWindow {
     
     private JFrame jframe;
+    private ImageIcon imageIcon;
     
     public MainWindow() {
+        jframe = setWindow(jframe);
+        jframe = setIcon(jframe, imageIcon);
+        displayWindow(jframe);
+    }
+
+    private static JFrame setWindow(JFrame jframe) {
         jframe = new JFrame("Flappy Bird");
-        jframe.setVisible(true);
         jframe.setSize(450, 800);
         jframe.setPreferredSize(new Dimension());
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setResizable(true);
+        return jframe;
     }
+
+    private static JFrame setIcon(JFrame jframe, ImageIcon imageIcon) {
+        String iconPath = Paths.get(System.getProperty("user.dir"), "/resources/favicon.ico").toString();
+        imageIcon = new ImageIcon();
+        System.out.println(iconPath);
+        jframe.setIconImage(imageIcon.getImage());
+        return jframe;
+    }
+
+    private static void displayWindow(JFrame jframe) {
+        jframe.setVisible(true);
+    }
+    
 }
