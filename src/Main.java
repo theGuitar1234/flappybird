9aa8b8d624178a90791ce51dc7bec512df2e1c6c
user <email>
2026-02-02 17:32:14 +0400

    a1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+class JFrame {
+
+    private static JFrame jframe;
+
+    public JFrame() {
+        jframe = new JFrame();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
